4aa48d2d8308e7173efb7cff1b6404a7d6bdc49c
zhengyang.shi <zhengyang.shi@ipinyou.com>
2020-05-26 01:20:45 +0800

    ts 语法学到readonly

--- learn-typescript/basic-type.ts
+++ learn-typescript/basic-type.ts
@@ -8,10 +8,31 @@ let firstName: string = '施正阳'
 
 let message:string = `Hello, ${firstName}, age is ${age}`
 
-console.log(message, "message");
+console.log(message, "message")
 
-let  u: undefined = undefined
+let  a: undefined = undefined
 
-let u: null = null;
+let b: null = null
 
-let num:number  = undefined;
+let num:number  = undefined
+
+// undefined 和 null 是所有数据类型的子类型
+
+let notSure: any = 4
+notSure = "shizhengyang"
+
+notSure.myName
+notSure.getName()
+
+let arrOfNumbers: number[] = [1,2,3,4];
+
+arrOfNumbers.push(4);
+
+let nums:Number = 5
+
+// 元组 Tuple 集合不同类型的数组
+
+let user: [string, number] = [ "1", 5];
+
+
+
