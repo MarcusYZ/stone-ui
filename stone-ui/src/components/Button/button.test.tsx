29ba3351308b72798998b2a221f207c8312a63a0
zhengyang.shi <zhengyang.shi@ipinyou.com>
2020-06-27 00:00:37 +0800

    组件测试

--- stone-ui/src/components/Button/button.test.tsx
+++ stone-ui/src/components/Button/button.test.tsx
@@ -2,11 +2,23 @@ import React from 'react'
 import { render } from '@testing-library/react'
 import Button from './button'
 
-test('case', () => {
-  const wrapper = render(<Button>Nice</Button>)
-  // 取得一个wrapper对象
-  const element = wrapper.queryByText('Nice')
-  // 在对象里找Nice节点
-  expect(element).toBeTruthy() 
-  // 希望这个值是true 
+
+
+// 最外层在组件做分类
+describe('test Button componentys', ()=> {
+  it('should render the corrent default button', () => {
+    const wrapper = render(<Button>Nice</Button>)
+    const element = wrapper.getByText('Nice')
+    expect(element).toBeInTheDocument()
+    // 在文档中
+    expect(element.tagName).toEqual('BUTTON')
+    expect(element).toHaveClass('btn btn-default')
+  })
+  it ('should render the corrent component based on different porps', () => {
+
+  })
+  it ('should render a link when btnType equals link and href is provided', () => {
+
+  })
+  // 写长一点描述便于自己和他人看懂用例
 })
